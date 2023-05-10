6f340871bf4db11f3d1a020f102b5b26ef76d582
Rishabhsingh78 <94614374+Rishabhsingh78@users.noreply.github.com>
2023-05-10 14:21:37 +0530

    Add files via upload

---
+++ Recipe/menus/views.py
@@ -0,0 +1,11 @@
+
+from rest_framework import viewsets
+
+from .serializers import MenusSerializer
+from .models import Menus
+
+# Create your views here.
+
+class MenusViewSet(viewsets.ModelViewSet):
+    queryset = Menus.objects.all().order_by('title')
+    serializer_class = MenusSerializer
