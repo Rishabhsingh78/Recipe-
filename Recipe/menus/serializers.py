81e422167660f6e22ae026541de265a83c00ed4b
Rishabhsingh78 <94614374+Rishabhsingh78@users.noreply.github.com>
2023-05-10 14:50:23 +0530

    Delete Recipe directory

--- Recipe/menus/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Menus
-
-class MenusSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta :
-        model = Menus
-        fields = ('title','description','ingredients','instrutions','perp_time','cook_time','created_at')
